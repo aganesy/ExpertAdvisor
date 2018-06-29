e65ea5e3917b7c5c5577170032862727329f688f
mi081321 <mi081321@gmail.com>
2018-06-29 22:55:09 +0900

    EAを所定に場所に配置するの面倒なので、シンボリックリンクを利用する。

---
+++ __Experts/mklink_experts.bat
@@ -0,0 +1,11 @@
+mklink test_position.ex4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position.ex4
+mklink test_position.mq4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position.mq4
+
+mklink test_position_clist.ex4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_clist.ex4
+mklink test_position_clist.mq4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_clist.mq4
+
+mklink test_position_mylist.ex4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_mylist.ex4
+mklink test_position_mylist.mq4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_mylist.mq4
+
+mklink test_position_template.ex4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_template.ex4
+mklink test_position_template.mq4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_template.mq4
