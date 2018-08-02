fd636610c39765c1a03bd9cd2b3502be36ecc20e
mi081321 <mi081321@gmail.com>
2018-08-02 21:59:42 +0900

    テストEAのex4のシンボリック作成バッチを更新

--- __Experts/mklink_experts.bat
+++ __Experts/mklink_experts.bat
@@ -9,3 +9,7 @@ mklink test_position_mylist.mq4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__t
 
 mklink test_position_template.ex4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_template.ex4
 mklink test_position_template.mq4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_position_template.mq4
+
+mklink test_file_write.ex4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_file_write.ex4
+mklink test_file_write.mq4 C:\Users\user\Documents\GitHub\ExpertAdvisor\__test\test_file_write.mq4
+
